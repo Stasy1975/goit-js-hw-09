758f45c2dfa079ad2bd813e0e87e077e89317363
Anastasiia Nedbai <nedbayfomina@gmail.com>
2022-08-11 18:34:03 +0300

    Switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStop.setAttribute('disabled', '');
+
+let intervalId = null;
+
+const changeColor = () =>
+  body.setAttribute('style', `background-color : ${getRandomHexColor()}`);
+
+const startSwitcher = () => {
+  btnStart.setAttribute('disabled', '');
+  btnStop.removeAttribute('disabled');
+  intervalId = setInterval(changeColor, 1000);
+};
+
+const stopSwitcher = () => {
+  clearInterval(intervalId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', '');
+};
+btnStart.addEventListener('click', startSwitcher);
+btnStop.addEventListener('click', stopSwitcher);
